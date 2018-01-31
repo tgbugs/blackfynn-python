00fce51face8cd1f0479eba3d4a46e5bb64df732
Misty Ahmed <misty@blackfynn.com>
2018-01-31 10:41:45 -0500

    track DataPackage owner

--- blackfynn/models.py
+++ blackfynn/models.py
@@ -673,7 +673,7 @@ class BaseCollection(BaseDataNode):
         if self.owner_id is not None:
             d['owner'] = self.owner_id
         return d
-    
+
     @classmethod
     def from_dict(cls, data, *args, **kwargs):
         item = super(BaseCollection, cls).from_dict(data, *args, **kwargs)
@@ -786,6 +786,12 @@ class DataPackage(BaseDataNode):
         """
         self._check_exists()
         return self._api.packages.get_view(self)
+
+    def as_dict(self):
+        d = super(DataPackage, self).as_dict()
+        if self.owner_id is not None:
+            d['owner'] = self.owner_id
+        return d
 
     @classmethod
     def from_dict(cls, data, *args, **kwargs):
